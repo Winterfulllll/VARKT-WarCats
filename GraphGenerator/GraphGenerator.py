40f1ab24cbf75a015212357a39945c155e0743f7
winterfulllll <masterfm65@gmail.com>
2023-12-05 22:03:20 +0300

    lol

---
+++ GraphGenerator/GraphGenerator.py
@@ -0,0 +1,79 @@
+# GraphGenerator by WarCats (Version 1.0.0)
+
+# Параметры, читаемые из файла:
+
+# 1) TimeSinceMark (Время)
+# 2) CurrentStage (X)
+# 3) AltitudeASL (X)
+# 4) DownRange (X)
+# 5) SpeedSurface (Скорость относительно Земли)
+# 6) SpeedOrbital (Орбитальная скорость)
+# 7) Mass (Масса)
+# 8) Acceleration (Ускорение) 
+# 9) Q (Удельная теплоемкость)
+# 10) AoA (X)
+# 11) AoS (X)
+# 12) AoD (X)
+# 13) AltitudeTrue (Высота)
+# 14) Pitch (X)
+# 15) GravityLosses (Гравитационные потери)
+# 16) DragLosses (Потери сопротивления)
+# 17) SteeringLosses (Потери управления)
+# 18) DeltaVExpended (Delta-V)
+# (X) - подразумевает, что данный параметр не будет использован
+
+import matplotlib.pyplot as plt
+
+# Вставить сюда название файла, записанного с помощью MechJeb2 (KSP mod):
+file = open("Muna1.csv")
+
+parameters = file.readline().rstrip("\n").split(",")
+parameters_count = len(parameters)
+data = {parameter: [] for parameter in parameters}
+
+parameters_rus = [ ("Время", 0), 
+        ("Скорость относительно Земли", 4),
+        ("Орбитальная скорость", 5),
+        ("Масса", 6),
+        ("Ускорение", 7),
+        ("Удельная теплоемкость", 8),
+        ("Высота", 12),
+        ("Гравитационные потери", 14),
+        ("Потери сопротивления", 15),
+        ("Потери управления", 16),
+        ("Delta-V", 17) ]
+
+# Чтение параметров и их значений из указанного файла
+for line in file:
+    values = line.rstrip("\n").split(",")
+    for i in range(parameters_count):
+        data[parameters[i]].append(float(values[i]))
+
+# Ввод пользователем значений для построения графика
+print(f'ВЫБЕРИТЕ 2 ПАРАМЕТРА ИЗ ПРИВЕДЕННЫХ НИЖЕ, ЧТОБЫ ПОСТРОИТЬ ГРАФИК:\n')
+
+parameters_rus_count = len(parameters_rus)
+for i in range(parameters_rus_count):
+    print(f'{i + 1}) {parameters_rus[i][0]}')
+print()
+
+parameter1 = int(input("ВВЕДИТЕ ПОРЯДКОВЫЙ НОМЕР ПЕРВОГО ПАРАМЕТР: ")) - 1
+parameter2 = int(input("ВВЕДИТЕ ПОРЯДКОВЫЙ НОМЕР ВТОРОГО ПАРАМЕТР: ")) - 1
+if (parameter1 <= -1) or (parameter1 >= parameters_rus_count) or \
+    (parameter2 <= -1) or (parameter2 >= parameters_rus_count):
+    raise ValueError("Incorrect parameters has been entered")
+
+# Получение значений из файла
+values1 = data[parameters[parameters_rus[parameter1][1]]]
+values2 = data[parameters[parameters_rus[parameter2][1]]]
+
+# Построение графика
+plt.plot(values1, values2)
+
+# Вставить сюда название для графика:
+plt.title(f'Название графика')
+plt.xlabel(parameters_rus[parameter1][0])
+plt.ylabel(parameters_rus[parameter2][0])
+
+# Сохранение графика
+plt.savefig('graphic.png')
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# VARKT-WarCats
-Repository of the WarCats team (MAI students of the M8O-107B-23 group) to work on the project on "Introduction to aviation and rocket and space technology"
+# ***WarCats Project***
+
+*Репозиторий* команды ***WarCats*** (учеников МАИ группы *М8О-107Б-23*) для работы над проектом по "Введение в авиационную и ракетно-космическую технику"
+
+
+*Миссия смоделирована в программе Kerbal Space Program*
+## *Отчёт*
+- [Документация по проекту]()
+- [Видео с полётом]()
+
+![Бим-бим](https://i.imgur.com/Z2Hcxes.png)
+
+## *Стек используемых технологий*
+
+- *Полёт на Луну* осуществлён с помощью модификации [*kOS*](https://ksp-kos.github.io/KOS/). Сбор игровых данных выполнен с помощью модификации [*MechJeb2*](https://www.curseforge.com/kerbal/ksp-mods/mechjeb).
+- Для *визуализации анализируемых данных* был использован язык программирования [*Python*](https://www.python.org) и библиотека [*matplotlib*](https://matplotlib.org).
+- Данные средства программирования позволяют начинающим программистам реализовать свои *графические задачи*, над которыми мы и работали.
